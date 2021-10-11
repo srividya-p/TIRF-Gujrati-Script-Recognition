613647626994ea5dacc84c29bdfb3dd2f2bf0d2a
Vineet Kekatpure <vineet.kekatpure@gmail.com>
2021-10-11 12:07:30 +0530

    wordninja added

--- autocorrect-options/test.py
+++ autocorrect-options/test.py
@@ -25,9 +25,21 @@ check = Speller(lang='en')
 print(check(input_str))
 print()
 
-from textblob import Word
-print("TextBlob with word segment")
-for word in wordbreak:
-    tb_word = Word(word)
-    print(tb_word.spellcheck())
+# from textblob import Word
+# print("TextBlob with word segment")
+# for word in wordbreak:
+#     tb_word = Word(word)
+#     print(tb_word.spellcheck())
+# print()
+
+print("WordNinja")
+import wordninja
+print(wordninja.split(input_str))
+print()
+
+print("Autocorrect with WordNinja")
+wn = wordninja.split(input_str)
+wn_sentence = ' '.join(str(x) for x in wn)
+check = Speller(lang="en")
+print(check(wn_sentence))
 print()
