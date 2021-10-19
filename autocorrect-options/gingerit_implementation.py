642291d177bb11f1911b6eb6e54f30ec5ff2d55e
Vineet Kekatpure <vineet.kekatpure@gmail.com>
2021-10-19 17:22:06 +0530

    gingerit implementation

--- autocorrect-options/gingerit_implementation.py
+++ autocorrect-options/gingerit_implementation.py
@@ -1,15 +1,6 @@
 from gingerit.gingerit import GingerIt
 
-text = 'The smelt of fliwers bring back memories.'
-
-parser = GingerIt()
-
-
-from gingerit.gingerit import GingerIt
-
 text = 'The project  aims at developing affine Handwritten Gujarati ScriptRecognitionsysem which can be effectively used for recognizing handwritten Gujarat in scripts'
-
-
 
 parser = GingerIt()
 parser.parse(text)
