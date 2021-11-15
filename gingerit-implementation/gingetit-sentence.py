0b41dbcb5f40d05709aef99bd469338c77dc3cff
Vineet Kekatpure <vineet.kekatpure@gmail.com>
2021-11-15 14:47:42 +0530

    bug fixes

--- gingerit-implementation/gingetit-sentence.py
+++ gingerit-implementation/gingetit-sentence.py
@@ -1,10 +1,16 @@
 import time
 from gingerit.gingerit import GingerIt
 
-ip = str(input("Input: \n"))
-constraint = 300
-print()
+#ip = str(input("Input: \n"))
 
+ip = '''The project  aims at developing affine Handwritten Gujarati Script
+Recognition sysem which can be effectively used for recognizing handwritten 
+Gujarat in scripts.The system wlltake an image  input which can be of anystandard format such as pg.nzetc, which will de then be pre-processed,then passed to the recognized which will extractthe text  nd 
+output through our app. The system wil table to recognize gujarat of different hand writings and wil save overall ime of physical to digital conversion'''
+
+constraint = 300
+
+ip = ip.replace("\n", " ")
 dividedInput = ip.split(".")
 
 unifiedOutput = ''
