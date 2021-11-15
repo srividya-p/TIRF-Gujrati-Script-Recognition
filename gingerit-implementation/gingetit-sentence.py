959ceb41595fd539d74852dbf6010b9f49108e6b
Vineet Kekatpure <vineet.kekatpure@gmail.com>
2021-11-15 12:01:33 +0530

    gingerit-bugfixes

--- gingerit-implementation/gingetit-sentence.py
+++ gingerit-implementation/gingetit-sentence.py
@@ -1,3 +1,4 @@
+import time
 from gingerit.gingerit import GingerIt
 
 ip = str(input("Input: \n"))
@@ -8,6 +9,7 @@ dividedInput = ip.split(".")
 
 unifiedOutput = ''
 
+begin = time.time()
 for i in range(len(dividedInput)):
     if(len(dividedInput[i]) >= 300):
         smallerInput = [(ip[i : i + constraint]) for i in range(0, len(dividedInput[i]), constraint)]
@@ -19,5 +21,9 @@ for i in range(len(dividedInput)):
         #print("Corrected Output:")
         unifiedOutput += parser.parse(dividedInput[i])['result']
 
+end = time.time()
+
 print("Corrected Output:")
 print(unifiedOutput)
+
+print("\n\n Time taken = ",str(end-begin), "s")
